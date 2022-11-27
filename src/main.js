76bb79f0c369f9bd587aac028158cca27384dc79
arthur <leroy1tramp@gmail.com>
2022-11-27 15:26:03 +0100

    Ajout d'un événement

--- src/main.js
+++ src/main.js
@@ -1,7 +1,44 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var calendarEl = document.getElementById('calendar');
-    var calendar = new FullCalendar.Calendar(calendarEl, {
-        initialView: 'dayGridMonth'
+main();
+
+function main(){
+    let calendar,
+    addButton;
+
+    initCalendar();
+}
+
+function initCalendar(){
+    document.addEventListener('DOMContentLoaded', function() {
+        let calendarEl = document.getElementById('calendar');
+        calendar = new FullCalendar.Calendar(calendarEl, {
+            initialView: 'dayGridMonth'
+        });
+        calendar.render();
+        addListeners();
+
     });
-    calendar.render();
-});
+}
+
+function addListeners() {
+    let addButton = document.getElementById('addBtn');
+    console.log(document);
+    if (addButton) {
+        addButton.addEventListener("click", addEvent, false);
+    }
+}
+
+function addEvent(){
+    let dateStr = prompt('Enter a date in YYYY-MM-DD format');
+    let date = new Date(dateStr + 'T00:00:00');
+
+    if (!isNaN(date.valueOf())) {
+        calendar.addEvent({
+            title: 'dynamic event',
+            start: date,
+            allDay: true
+        });
+        alert('évènement ajouté');
+    } else {
+        alert('date incorrect');
+    }
+}
