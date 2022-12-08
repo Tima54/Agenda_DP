09962841bfbaf3811c70b185ab893fdf54e6f27a
arthur <leroy1tramp@gmail.com>
2022-12-08 17:17:20 +0100

    Ajout d'un événement en cliquant sur une cellule

--- src/main.js
+++ src/main.js
@@ -11,7 +11,11 @@ function initCalendar(){
     document.addEventListener('DOMContentLoaded', function() {
         let calendarEl = document.getElementById('calendar');
         calendar = new FullCalendar.Calendar(calendarEl, {
-            initialView: 'dayGridMonth'
+            initialView: 'dayGridMonth',
+            selectable: true,
+            dateClick: function (info){
+                addEvent(info.dateStr);
+            }
         });
         calendar.render();
         addListeners();
@@ -19,34 +23,13 @@ function initCalendar(){
 }
 
 function addListeners() {
-    let addButton = document.getElementById('addBtn');
-    if (addButton) {
-        addButton.addEventListener("click", addEvent, false);
-    }
     let cells = document.querySelectorAll('[role="gridcell"]');
-    cells.forEach(element => element.addEventListener("click",selectCell,false));
 }
 
-function selectCell(){
-    if(this.classList.contains("selected-cell")){
-        this.classList.remove("selected-cell");
-    }else{
-        this.classList.add("selected-cell");
-    }
-}
-
-function addEvent(){
-    let dateStr = prompt('Enter a date in YYYY-MM-DD format');
-    let date = new Date(dateStr + 'T00:00:00');
-
-    if (!isNaN(date.valueOf())) {
-        calendar.addEvent({
-            title: 'dynamic event',
-            start: date,
-            allDay: true
-        });
-        alert('évènement ajouté');
-    } else {
-        alert('date incorrect');
-    }
+function addEvent(date){
+    calendar.addEvent({
+        title: 'dynamic event',
+        start: date,
+        allDay: true
+    });
 }
