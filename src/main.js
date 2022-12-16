e9384ade2f506318bc4fa090e50785ecf8f34fcb
Thomas Bohan <thomas.bohan@cirkwi.com>
2022-12-16 13:52:10 +0100

    Ajout pop up pour création et modification event

--- src/main.js
+++ src/main.js
@@ -18,12 +18,15 @@ function initCalendar(){
                 center:'title',
                 right:'dayGridMonth,timeGridWeek'
             },
-            dateClick: function(info){
-                addEvent(info.dateStr);
+            dateClick: function(){
+                document.getElementById('modal').style.display = 'block'
+                //addEvent(info.dateStr);
             },
             eventClick: function(info){
-                let val = prompt("Nouveau titre :");
-                info.event.setProp('title',val);
+                document.getElementById('modal_ModifEvent').style.display = 'block'
+                document.getElementById('validerEdit').addEventListener("click",function(){editEvent(info)});
+                //let val = prompt("Nouveau titre :");
+                //info.event.setProp('title',val);
             }
         });
         calendar.render();
@@ -34,11 +37,45 @@ function initCalendar(){
 function addListeners() {
 }
 
-function addEvent(date){
-    let desc = prompt("Titre de l'événement :");
+/**
+ * Cette fonction permet la création d'un événement dans le calendrier
+ */
+function addEvent(){
+    let title = document.getElementById('name').value;
+    let startDate = document.getElementById('startDate').value;
+    let endDate = document.getElementById('endDate').value;
     calendar.addEvent({
-        title: desc,
-        start: date,
-        //allDay: true
+        title: title,
+        start: startDate,
+        end: endDate
     });
+    fermerModal();
+}
+
+/**
+ * Cette fonction permet la modification de l'événement selectionner auparavant
+ * @param info : il s'agit de l'événement vers lequel on pointe
+ */
+function editEvent(info){
+    let title = document.getElementById('editName').value;
+    let startDate = document.getElementById('editStartDate').value;
+    let endDate = document.getElementById('editEndDate').value;
+    info.event.setProp('title',title);
+    info.event.setStart(startDate);
+    info.event.setEnd(endDate);
+    fermerModalModifEvent();
+}
+
+/**
+ * Cette fonction permet de fermer la pop-up de création d'un événement
+ */
+function fermerModal(){
+    document.getElementById('modal').style.display = 'none'
+}
+
+/**
+ * Cette fonction permet de fermer la pop-up de modification d'un événement
+ */
+function fermerModalModifEvent(){
+    document.getElementById('modal_ModifEvent').style.display = 'none'
 }
